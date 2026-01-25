8e43bbe3239f7e82223d9e883c58e281b7f71c39
Oleksandr K. <kucherenko.alex@gmail.com>
2026-01-25 21:19:00 +0100

    fix: preserve repo structure, install to libexec, symlink bin

--- Formula/e-bash.rb
+++ Formula/e-bash.rb
@@ -5,38 +5,32 @@ class EBash < Formula
   sha256 "b5ce1c797750ed0c1533665bc48650266cdd991c259399412a3afe0f6bd7f69b"
   license "MIT"
 
-
-
   depends_on "bash"
   depends_on "coreutils"
-  depends_on "gawk"
   depends_on "git"
+  depends_on "gawk"
   depends_on "gnu-sed"
   depends_on "grep"
   depends_on "jq"
 
   def install
-    # Install core scripts to libexec (private)
-    libexec.install Dir[".scripts/*"]
+    # Install everything to libexec to preserve structure
+    libexec.install Dir["*"]
+    libexec.install ".scripts" if Dir.exist?(".scripts")
 
-    # Install binaries
-    bin.install Dir["bin/*"].reject { |f| File.directory?(f) }
-
-    # Documentation and Demos
-    doc.install "README.md", "LICENSE"
-    (pkgshare/"demos").install Dir["demos/*"] if Dir.exist?("demos")
+    # Install binaries as symlinks
+    bin.install_symlink Dir[libexec/"bin/*"]
   end
 
   def caveats
     <<~EOS
       e-bash is installed to #{libexec}.
       Add the following to your shell profile:
-        export E_BASH="#{opt_libexec}"
+        export E_BASH="#{opt_libexec}/.scripts"
     EOS
   end
 
   test do
-    assert_predicate libexec/"_logger.sh", :exist?
-    system "#{bin}/e-bash", "help"
+    assert_predicate libexec/".scripts/_logger.sh", :exist?
   end
 end
